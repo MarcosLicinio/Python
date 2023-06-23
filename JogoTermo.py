b0eb83bb09f9cd908a83df9106061956aee4e8c9
MarcosLicinio <137295529+MarcosLicinio@users.noreply.github.com>
2023-06-23 11:05:44 -0300

    Update JogoTermo.py

--- JogoTermo.py
+++ JogoTermo.py
@@ -1,46 +1,59 @@
 import os
-palavrasecreta = 'carro'
 caracter = '*'
 letrasacertadas = ''
 contador = 0
-print('- - Bem vindo ao jogo TERMO versão Pythoresca - -')
+opcoesescolhidas = ''
+lista = ['carro', 'casa', 'boneca', 'perfume', 'janela', 'sofá', 'montanha', 'nuvem', 'cavalo', 'mesa']
 
-while True:
-    print(f'Você tem {(10 - contador)} tentativas')
-    letradigitada = input('Digite uma letra: ')
-    contador = contador + 1
+while True: 
 
-    if len(letradigitada) > 1:
-        print('Digite apenas uma letra')
+    print('- - Bem vindo ao jogo TERMO versão Pythoresca - -')
+    opcao_escolhida = input('Digite de 1 a 10 para escolher uma palavra secreta: ')
+
+    if opcao_escolhida in opcoesescolhidas:
+        print('Você já escolheu esta palavra')
         continue
 
-    if letradigitada in palavrasecreta:
-        letrasacertadas += letradigitada
-    
-    palavraacertada = ''
+    palavrasecreta = lista[(int(opcao_escolhida) - 1)]
+    opcoesescolhidas += opcao_escolhida
 
-    for letrasecreta in palavrasecreta:
-        if letrasecreta in letrasacertadas:
-            palavraacertada += letrasecreta
-        else:
-            palavraacertada += caracter
-    
-    print(palavraacertada)
+    while True:
 
-    if palavrasecreta == palavraacertada:
-        print('Parabéns! Você acertou a palavra!')
-        letradigitada = ''
+        print(f'Você tem {(10 - contador)} tentativas')
+        letradigitada = input('Digite uma letra: ')
+        contador = contador + 1
+
+        if len(letradigitada) > 1:
+            print('Digite apenas uma letra')
+            continue
+
+        if letradigitada in palavrasecreta:
+            letrasacertadas += letradigitada
+        
         palavraacertada = ''
-        letrasacertadas = ''
-        contador = 0
-        os.system('cls')
-        print('Jogo reiniciado')
 
-    if contador >= 10:
-        print('Você atingiu o máximo de tentativas, o jogo será reiniciado')
-        letradigitada = ''
-        palavraacertada = ''
-        letrasacertadas = ''
-        contador = 0
-        os.system('cls')
-        continue
+        for letrasecreta in palavrasecreta:
+            if letrasecreta in letrasacertadas:
+                palavraacertada += letrasecreta
+            else:
+                palavraacertada += caracter
+        
+        print(palavraacertada)
+
+        if palavrasecreta == palavraacertada:
+            os.system('cls')
+            print('Parabéns! Você acertou a palavra!')
+            letradigitada = ''
+            palavraacertada = ''
+            letrasacertadas = ''
+            contador = 0
+            break
+
+        if contador >= 10:
+            os.system('cls')
+            print('Você atingiu o máximo de tentativas, o jogo será reiniciado')
+            letradigitada = ''
+            palavraacertada = ''
+            letrasacertadas = ''
+            contador = 0
+            break
